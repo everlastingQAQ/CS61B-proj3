4a41a2c3ba2a5f4f834c75e01897fe11ec77d650
everlastingQAQ <2279839810@qq.com>
2026-09-03 12:45:35 +0800

    almost finish

--- byow/Core/WorldGenerator/RegionConnector.java
+++ byow/Core/WorldGenerator/RegionConnector.java
@@ -9,7 +9,14 @@ import static byow.Core.Engine.HEIGHT;
 import static byow.Core.Engine.WIDTH;
 import static java.util.Collections.shuffle;
 
+/**
+ * 用于连接地图的各个部分，使地图成为一个完整的连通块
+ *
+ * 通过 {@link #connect(TETile[][], int[][], Random)} 方法连接地图。
+ * */
 public class RegionConnector {
+
+    private static final int EXTRA_CONNECTOR_CHANCE = 50;
 
     private static final int[][] DIRS = {
             {1, 0},
@@ -18,27 +25,53 @@ public class RegionConnector {
             {0, -1}
     };
 
-    private List<Connecter> connectorPositions = new ArrayList<>();
+    private final List<Connector> connectorPositions = new ArrayList<>();
 
-    public void connect(TETile[][] world, int[][] regions, int roomSize, Random random) {
+    /**
+     * 将整张地图的房间和迷宫串联起来
+     *
+     * @param world 将要串联起来的地图
+     * @param regions 每个格子的连通块编号
+     * @param random 随机数生成器
+     * */
+    public void connect(TETile[][] world, int[][] regions, Random random) {
+        // 统计可能会变成门的格子
         updatePos(world, regions);
 
-        int regionNums = roomSize;
+        int regionNums = getRegionSize();
         shuffle(connectorPositions, random);
 
         DSU dsu = new DSU(regionNums + 1);
-        for (Connecter connecter : connectorPositions) {
-            int first = connecter.regions.first();
+
+        // 遍历可能会变成门的格子，通过并查集来判断是否应该变成门
+        for (Connector connector : connectorPositions) {
+            TreeSet<Integer> set = connector.regions;
+            int first = set.first();
+            boolean connected = false;
+            for (int x : set) {
+                if (dsu.merge(first, x)) {
+                    connected = true;
+                }
+            }
+            if (connected || random.nextInt(EXTRA_CONNECTOR_CHANCE) == 0) {
+                world[connector.x][connector.y] = Tileset.UNLOCKED_DOOR;
+            }
         }
     }
 
+    /**
+     * 统计可能会被变成门的格子，并更新{@link connectorPositions}
+     *
+     * @param world 将要串联起来的地图
+     * @param regions 每个格子的连通块编号
+     * */
     private void updatePos(TETile[][] world, int[][] regions) {
         for (int i = 0; i < WIDTH; i++) {
             for (int j = 0; j < HEIGHT; j++) {
                 if (!world[i][j].equals(Tileset.WALL)) {
                     continue;
                 }
-                TreeSet<Integer> regionCounts = new TreeSet<Integer>();
+                TreeSet<Integer> regionCounts = new TreeSet<>();
 
                 for (int[] dir : DIRS) {
                     int x = i + dir[0];
@@ -53,18 +86,18 @@ public class RegionConnector {
                 }
 
                 if (regionCounts.size() >= 2) {
-                    this.connectorPositions.add(new Connecter(i, j, regionCounts));
+                    this.connectorPositions.add(new Connector(i, j, regionCounts));
                 }
             }
         }
     }
 
-    private static class Connecter {
-        private int x;
-        private int y;
-        private TreeSet<Integer> regions;
+    private static class Connector {
+        private final int x;
+        private final int y;
+        private final TreeSet<Integer> regions;
 
-        Connecter(int x, int y, TreeSet<Integer> regions) {
+        Connector(int x, int y, TreeSet<Integer> regions) {
             this.x = x;
             this.y = y;
             this.regions = regions;
@@ -84,8 +117,8 @@ public class RegionConnector {
     }
 
     private static class DSU {
-        private int[] parent;
-        private int[] size;
+        private final int[] parent;
+        private final int[] size;
 
         DSU(int n) {
             parent = new int[n];
@@ -104,7 +137,7 @@ public class RegionConnector {
             return parent[x];
         }
 
-        boolean union(int a, int b) {
+        boolean merge(int a, int b) {
             a = find(a);
             b = find(b);
             if (a == b) {
