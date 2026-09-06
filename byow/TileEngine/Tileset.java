8e9a82a579c5e9c4a1c30bf2f5c6980afeb0ded1
everlastingQAQ <2279839810@qq.com>
2026-09-06 15:17:33 +0800

    add some tileSets

--- byow/TileEngine/Tileset.java
+++ byow/TileEngine/Tileset.java
@@ -33,6 +33,9 @@ public class Tileset {
     public static final TETile SAND = new TETile('▒', Color.yellow, Color.black, "sand");
     public static final TETile MOUNTAIN = new TETile('▲', Color.gray, Color.black, "mountain");
     public static final TETile TREE = new TETile('♠', Color.green, Color.black, "tree");
+    public static final TETile ROCK = new TETile('●', Color.gray, Color.black, "rock");
+    public static final TETile BRIDGE = new TETile('=',new Color(139, 69, 19), Color.black,
+            "bridge");
 }
 
 
